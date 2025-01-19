9318aac53bb5add13a5ea97ee18b0c9a8574b5c4
Svetlana Chernysheva <chers1303@gmail.com>
2025-01-19 16:07:53 +0400

    change settings.py for rollbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -58,7 +58,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
+
+ROLLBAR = {
+    'access_token': '8353d596dccc440a8ddd6a0a26927257',
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = 'task_manager.urls'
 
