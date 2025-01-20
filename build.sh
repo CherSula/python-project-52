f34489e40c4e97923b088f84d1d416b5e355d849
Svetlana Chernysheva <chers1303@gmail.com>
2025-01-20 15:23:52 +0400

    change build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Exit on error
-set -o errexit
+# set -o errexit
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 uv sync
 
-# Convert static asset files
-uv run python3 manage.py collectstatic --no-input
+# # Convert static asset files
+# uv run python3 manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
-uv run python3 manage.py migrate
+# # Apply any outstanding database migrations
+# uv run python3 manage.py migrate
