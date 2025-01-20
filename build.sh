63b66963d82dd30f69b3f33db53fcd2960c8a52b
Svetlana Chernysheva <chers1303@gmail.com>
2025-01-20 15:31:28 +0400

    change back build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 uv sync
 
-# # Convert static asset files
-# uv run python3 manage.py collectstatic --no-input
+# Convert static asset files
+uv run python3 manage.py collectstatic --no-input
 
-# # Apply any outstanding database migrations
-# uv run python3 manage.py migrate
+# Apply any outstanding database migrations
+uv run python3 manage.py migrate
